c58cd3a4c2111198cfa6035fb74ca83a3f5c0e78
Diana Koval <dianakoval@Dianas-MacBook-Pro.local>
2025-01-16 18:36:59 +0200

    fix(TASK-3): Fixed UI bugs

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -1,5 +1,7 @@
-import React, { ForwardRefRenderFunction } from "react";
+import React, { ForwardRefRenderFunction, useState } from "react";
 import { FieldError } from "react-hook-form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import classes from "./Input.module.scss";
 import cn from "classnames";
 
@@ -18,9 +20,25 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ placeholder, type, validationConditions, error, showErrorOnInvalid, ...props }, ref) => {
+  const [isShownPassword, setShownPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShownPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
     <div>
-      <input type={type} className={cn(classes.input, {[classes.isError]: !!error})} placeholder={placeholder} ref={ref} {...props} />
+      <div className={classes.input__container}>
+        <input type={isShownPassword ? "text" : type} className={cn(classes.input, {[classes.isError]: !!error})} placeholder={placeholder} ref={ref} {...props} />
+        {type === "password" && (
+          <FontAwesomeIcon
+            size="sm"
+            icon={isShownPassword ? faEye : faEyeSlash}
+            className={cn(classes.input__toggleIcon, {[classes.isError]: !!error})}
+            onClick={togglePasswordVisibility}
+          />
+        )}
+      </div>
       {error && !validationConditions && <p className={classes.input__error}>{error.message}</p>}
 
       {validationConditions && (
